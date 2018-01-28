5204847df54a7498bfc80d1908969d9f8af1c2a7
Asiryan Alexander <alichek95@mail.ru>
2018-01-29 00:17:09 +0300

    Add kata "Find the missing letter"

---
+++ katas/Python/6 kyu/Find the missing letter 5839edaa6754d6fec10000a2.py
@@ -0,0 +1,5 @@
+# https://www.codewars.com/kata/5839edaa6754d6fec10000a2
+def find_missing_letter(chars):
+    for i in range(len(chars) - 1):
+        if ord(chars[i]) != ord(chars[i + 1]) - 1:
+            return chr(ord(chars[i]) + 1)
