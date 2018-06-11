c8cf570630e05bd0b93fa353fe2ec061e87a70ea
Asiryan Alexander <alichek95@mail.ru>
2018-06-11 13:28:34 +0300

    Add kata "Sleigh Authentication"

---
+++ katas/Python/8 kyu/Sleigh Authentication 52adc142b2651f25a8000643.py
@@ -0,0 +1,4 @@
+# https://www.codewars.com/kata/52adc142b2651f25a8000643
+class Sleigh(object):
+    def authenticate(self, name, password):
+        return name == 'Santa Claus' and password == 'Ho Ho Ho!'
