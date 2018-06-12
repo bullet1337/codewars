142d44d3fdda5f9343a7cf405a961f2df9055aaf
Asiryan Alexander <alichek95@mail.ru>
2018-06-12 21:13:23 +0300

    Add kata "Switch it Up!"

---
+++ katas/Python/8 kyu/Switch it Up 5808dcb8f0ed42ae34000031.py
@@ -0,0 +1,3 @@
+# https://www.codewars.com/kata/5808dcb8f0ed42ae34000031
+def switch_it_up(number):
+    return ['Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'][number]
