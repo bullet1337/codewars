4e4092123df6c966acdba964ed589d370c5ee4bb
Asiryan Alexander <alichek95@mail.ru>
2018-06-14 01:30:06 +0300

    Add kata "Get Planet Name By ID"

---
+++ katas/Python/8 kyu/Get Planet Name By ID 515e188a311df01cba000003.py
@@ -0,0 +1,3 @@
+# https://www.codewars.com/kata/515e188a311df01cba000003
+def get_planet_name(id):
+    return ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'][id - 1]
