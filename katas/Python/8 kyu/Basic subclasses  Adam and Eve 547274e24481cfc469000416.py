62c565a84304a77caa6f1075e750db890dfc8fe2
Asiryan Alexander <alichek95@mail.ru>
2018-06-17 23:55:10 +0300

    Add kata "Basic subclasses - Adam and Eve"

---
+++ katas/Python/8 kyu/Basic subclasses  Adam and Eve 547274e24481cfc469000416.py
@@ -0,0 +1,13 @@
+# https://www.codewars.com/kata/547274e24481cfc469000416
+def God():
+    return [Man(), Woman()]
+
+
+class Human:
+    pass
+
+class Man(Human):
+    pass
+
+class Woman (Human):
+    pass
