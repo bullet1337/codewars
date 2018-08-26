42a7cb0b97f606e1876d5cb203840a30ed84c71c
Asiryan Alexander <alichek95@mail.ru>
2018-08-26 22:42:13 +0300

    Add kata "If you can read this..."

---
+++ katas/Python/6 kyu/If you can read this 586538146b56991861000293.py
@@ -0,0 +1,35 @@
+# https://www.codewars.com/kata/586538146b56991861000293
+import re
+
+alphabet = {
+    'A': 'Alfa',
+    'B': 'Bravo',
+    'C': 'Charlie',
+    'D': 'Delta',
+    'E': 'Echo',
+    'F': 'Foxtrot',
+    'G': 'Golf',
+    'H': 'Hotel',
+    'I': 'India',
+    'J': 'Juliett',
+    'K': 'Kilo',
+    'L': 'Lima',
+    'M': 'Mike',
+    'N': 'November',
+    'O': 'Oscar',
+    'P': 'Papa',
+    'Q': 'Quebec',
+    'R': 'Romeo',
+    'S': 'Sierra',
+    'T': 'Tango',
+    'U': 'Uniform',
+    'V': 'Victor',
+    'W': 'Whiskey',
+    'X': 'Xray',
+    'Y': 'Yankee',
+    'Z': 'Zulu',
+}
+
+
+def to_nato(words):
+    return ' '.join(alphabet[l.upper()] if l.isalpha() else l for l in words if not l.isspace())
