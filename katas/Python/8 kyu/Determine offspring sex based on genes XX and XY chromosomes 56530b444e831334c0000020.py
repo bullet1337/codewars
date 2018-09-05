b1425050da6bb36634456c610a62318f572a8ecb
Asiryan Alexander <alichek95@mail.ru>
2018-09-05 23:25:03 +0300

    Add kata "Determine offspring sex based on genes XX and XY chromosomes"

---
+++ katas/Python/8 kyu/Determine offspring sex based on genes XX and XY chromosomes 56530b444e831334c0000020.py
@@ -0,0 +1,3 @@
+# https://www.codewars.com/kata/56530b444e831334c0000020
+def chromosome_check(sperm):
+    return 'Congratulations! You\'re going to have a {}.'.format('son' if sperm[1] == 'Y' else 'daughter')
