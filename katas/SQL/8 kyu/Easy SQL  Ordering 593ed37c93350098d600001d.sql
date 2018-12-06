3ee355aa2c5ccf4811627ce0e8b7d01a9f997db1
Asiryan Alexander <alichek95@mail.ru>
2018-12-06 23:57:33 +0300

    Add kata "Easy SQL - Ordering"

---
+++ katas/SQL/8 kyu/Easy SQL  Ordering 593ed37c93350098d600001d.sql
@@ -0,0 +1,4 @@
+-- https://www.codewars.com/kata/593ed37c93350098d600001d
+select *
+from companies
+order by employees desc
---
+++ katas/SQL/8 kyu/Register for the Party SQL for Beginners 3 590cc86f7557c0494000007e.sql
@@ -0,0 +1,5 @@
+-- https://www.codewars.com/kata/590cc86f7557c0494000007e
+insert into participants values ('Alexander', 23, true);
+
+select * from participants;
+
