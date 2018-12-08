b6e79a7f002098643c8959ce9af3ecfaffc2d3c0
Asiryan Alexander <alichek95@mail.ru>
2018-12-09 00:24:34 +0300

    Add kata "Conditional Count"

---
+++ katas/SQL/6 kyu/Conditional Count 5816a3ecf54413a113000074.sql
@@ -0,0 +1,13 @@
+-- https://www.codewars.com/kata/5816a3ecf54413a113000074
+select 
+  date_part('month', payment_date) as month, 
+  count(*) as total_count,
+  sum(amount) as total_amount,
+  count(*) filter(where staff_id = 1) as mike_count,
+  sum(amount) filter(where staff_id = 1) as mike_amount,
+  count(*) filter(where staff_id = 2) as jon_count,
+  sum(amount) filter(where staff_id = 2) as jon_amount
+from payment 
+group by month
+order by month
+  
