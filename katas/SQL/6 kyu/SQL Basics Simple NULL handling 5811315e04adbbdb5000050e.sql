f5b0c29c33ec49b940b1bf5e9dad93c7644df47e
Asiryan Alexander <alichek95@mail.ru>
2018-12-08 23:56:02 +0300

    Add kata "SQL Basics: Simple NULL handling"

---
+++ katas/SQL/6 kyu/SQL Basics Simple NULL handling 5811315e04adbbdb5000050e.sql
@@ -0,0 +1,10 @@
+-- https://www.codewars.com/kata/5811315e04adbbdb5000050e
+select 
+  id,
+  coalesce(nullif(name, ''), '[product name not found]') as name,
+  price,
+  coalesce(nullif(card_name , ''), '[card name not found]') as card_name,
+  card_number,
+  transaction_date
+from eusales
+where price > 50
